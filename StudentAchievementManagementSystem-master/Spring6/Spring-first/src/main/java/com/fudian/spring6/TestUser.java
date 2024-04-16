ef9b9512d771a13efacaed3b294bada91b863893
gitCaidian618 <git204705487@qq.com>
2024-04-16 20:21:55 +0800

    student system

---
+++ StudentAchievementManagementSystem-master/Spring6/Spring-first/src/main/java/com/fudian/spring6/TestUser.java
@@ -0,0 +1,18 @@
+package com.fudian.spring6;
+
+import javafx.application.Application;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestUser {
+    @test
+//    public void testUserObject(){
+////        Application context =new ClassPathXmlApplicationContext("bean.xml");
+////        User user =(User)context.getBean( name:"user");System.out.println(user);
+//////使用对象调用方法进行测试
+////        user.add();
+//        System.out.println("add.....");
+//    }
+    public public static void main(String[] args) {
+        System.out.println("add.....");
+    }
+}
